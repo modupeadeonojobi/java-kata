b664c21b74a1e218af2dffeb49c211322c27867e
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-07-28 13:29:08 +0100

    Add assign partner

--- src/com/imodupsy/agiletpd/funtime/AssignPartner.java
+++ src/com/imodupsy/agiletpd/funtime/AssignPartner.java
@@ -8,10 +8,37 @@ import java.util.*;
  */
 public class AssignPartner {
 
+
     public static void main(String[] args) {
-//done
+        pick();
     }
 
+    public static void pick() {
+        List<String> nameList = new ArrayList<>();
+        nameList.add("Modupe");
+        nameList.add("Chijioke");
+        nameList.add("David");
+        nameList.add("Tao");
+        nameList.add("Mary");
+        nameList.add("Eze");
+        nameList.add("Ihe");
+
+        List<String[]> paired = new ArrayList<>();
+
+        Collections.shuffle(nameList);
+        for (int i = 0; i < nameList.size() - 1 ; i++) {
+            if (i == 0 && nameList.size() % 2 != 0) {
+                String[] namePair = {nameList.get(i),  nameList.get(i+=1),  nameList.get(i+=2)};
+                paired.add(namePair);
+                System.out.println(Arrays.toString(namePair));
+            }
+            String[] namePair = {nameList.get(i),  nameList.get(i+=1)};
+            paired.add(namePair);
+            System.out.println(Arrays.toString(namePair));
+
+        }
+
+    }
 
 
 }
