3507993233ad8c010862bc8099114eef6f0534c0
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-07-29 13:03:03 +0100

    Update assignPartner method

--- src/com/imodupsy/agiletpd/funtime/AssignPartner.java
+++ src/com/imodupsy/agiletpd/funtime/AssignPartner.java
@@ -1,13 +1,15 @@
 package com.imodupsy.agiletpd.funtime;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author iModupsy
- * @created 27/07/2022
+ * @created 29/07/2022
  */
 public class AssignPartner {
-
 
     public static void main(String[] args) {
         pick();
@@ -23,21 +25,35 @@ public class AssignPartner {
         nameList.add("Eze");
         nameList.add("Chinedu");
 
-        List<String[]> paired = new ArrayList<>();
 
         Collections.shuffle(nameList);
         for (int i = 0; i < nameList.size() - 1 ; i++) {
             if (i == 0 && nameList.size() % 2 != 0) {
-                String[] namePair = {nameList.get(i),  nameList.get(i+=1),  nameList.get(i+=2)};
-                paired.add(namePair);
+                String[] namePair = {nameList.get(0), nameList.get(1), nameList.get(2)};
                 System.out.println(Arrays.toString(namePair));
+                i = 3;
             }
-            String[] namePair = {nameList.get(i),  nameList.get(i+=1)};
-            paired.add(namePair);
+            String[] namePair = {nameList.get(i), nameList.get(i += 1)};
             System.out.println(Arrays.toString(namePair));
-
         }
 
+        /**
+         * SECOND SOLUTION
+         */
+
+//        int loopIndex = 0;
+//        if (nameList.size() % 2 != 0) {
+//            String[] namePair = {nameList.get(0), nameList.get(1), nameList.get(2)};
+//            paired.add(namePair);
+//            System.out.println(Arrays.toString(namePair));
+//            loopIndex = 3;
+//        }
+//
+//        for (int i = loopIndex; i < nameList.size() - 1 ; i+=2) {
+//            String[] namePair = {nameList.get(i), nameList.get(i + 1)};
+//            paired.add(namePair);
+//            System.out.println(Arrays.toString(namePair));
+//        }
     }
 
 
