3df16473316e0a5a73c0e4a290eb388001b5e018
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-07-30 21:17:18 +0100

    Add primeFactors question

---
+++ src/com/imodupsy/agiletpd/classicaltdd3/PrimeFactors.java
@@ -0,0 +1,13 @@
+package com.imodupsy.agiletpd.classicaltdd3;
+
+/**
+ * @author iModupsy
+ * @created 30/07/2022
+ */
+public class PrimeFactors {
+
+    /**
+     * Factorize a positive integer number into its prime factors using TDD and the TPP table.
+     * 2, 3, 4, 6, 9, 12, 15
+     */
+}
