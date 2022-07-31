b1f7719d4e67077cdcb8a9e6a7efc7e4ffa47b38
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-07-31 22:18:55 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/com/imodupsy/agiletpd/classicaltdd3/PrimeFactorsTest.java

--- src/com/imodupsy/agiletpd/classicaltdd3/PrimeFactorsTest.java
+++ src/com/imodupsy/agiletpd/classicaltdd3/PrimeFactorsTest.java
@@ -2,6 +2,8 @@ package com.imodupsy.agiletpd.classicaltdd3;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * @author iModupsy
  * @created 31/07/2022
@@ -12,6 +14,6 @@ public class PrimeFactorsTest {
     public void one_isEqualToAnEmptyArray() {
         PrimeFactors primeFactors = new PrimeFactors();
         int [] primeNumbers = primeFactors.of(1);
-        assertEquals(1, value);
+        assertEquals(1, primeNumbers);
     }
 }
